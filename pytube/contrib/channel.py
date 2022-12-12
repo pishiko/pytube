60689aa7d905c99bba3d88b5fa48c2d8f4dff66e
pishiko <42219754+pishiko@users.noreply.github.com>
2022-12-13 05:56:39 +0900

    add shorts

--- pytube/contrib/channel.py
+++ pytube/contrib/channel.py
@@ -29,6 +29,7 @@ class Channel(Playlist):
 
         self.videos_url = self.channel_url + '/videos'
         self.streams_url = self.channel_url + '/streams'
+        self.shorts_url = self.channel_url + '/shorts'
         self.playlists_url = self.channel_url + '/playlists'
         self.community_url = self.channel_url + '/community'
         self.featured_channels_url = self.channel_url + '/channels'
@@ -36,6 +37,7 @@ class Channel(Playlist):
 
         # Possible future additions
         self._streams_html = None
+        self._shorts_html = None
         self._playlists_html = None
         self._community_html = None
         self._featured_channels_html = None
@@ -90,6 +92,17 @@ class Channel(Playlist):
             return self._streams_html
         self._streams_html = request.get(self.streams_url)
         return self._streams_html
+
+    @property
+    def shorts_html(self):
+        """Get the html for the /shorts page.
+
+        :rtype: str
+        """
+        if self._shorts_html:
+            return self._shorts_html
+        self._shorts_html = request.get(self.shorts_url)
+        return self._shorts_html
 
     @property
     def playlists_html(self):
@@ -293,3 +306,86 @@ class Channel(Playlist):
         :returns: List of video URLs
         """
         return DeferredGeneratorList(self.url_generator(extractor=self._extract_streams,html=self.streams_html))
+
+    @staticmethod
+    def _extract_shorts(raw_json: str) -> Tuple[List[str], Optional[str]]:
+        """Extracts shorts from a raw json page
+
+        :param str raw_json: Input json extracted from the page or the last
+            server response
+        :rtype: Tuple[List[str], Optional[str]]
+        :returns: Tuple containing a list of up to 100 video watch ids and
+            a continuation token, if more videos are available
+        """
+        initial_data = json.loads(raw_json)
+        # this is the json tree structure, if the json was extracted from
+        # html
+        try:
+            videos = initial_data["contents"][
+                "twoColumnBrowseResultsRenderer"][
+                "tabs"][2]["tabRenderer"]["content"][
+                "richGridRenderer"]["contents"]
+        except (KeyError, IndexError, TypeError):
+            try:
+                # this is the json tree structure, if the json was directly sent
+                # by the server in a continuation response
+                important_content = initial_data[1]['response']['onResponseReceivedActions'][
+                    0
+                ]['appendContinuationItemsAction']['continuationItems']
+                videos = important_content
+            except (KeyError, IndexError, TypeError):
+                try:
+                    # this is the json tree structure, if the json was directly sent
+                    # by the server in a continuation response
+                    # no longer a list and no longer has the "response" key
+                    important_content = initial_data['onResponseReceivedActions'][0][
+                        'appendContinuationItemsAction']['continuationItems']
+                    videos = important_content
+                except (KeyError, IndexError, TypeError) as p:
+                    logger.info(p)
+                    return [], None
+
+        try:
+            continuation = videos[-1]['continuationItemRenderer'][
+                'continuationEndpoint'
+            ]['continuationCommand']['token']
+            videos = videos[:-1]
+        except (KeyError, IndexError):
+            # if there is an error, no continuation is available
+            continuation = None
+
+        # remove duplicates
+        return (
+            uniqueify(
+                list(
+                    # only extract the video ids from the video data
+                    map(
+                        lambda x: (
+                            f"/watch?v="
+                            f"{x['richItemRenderer']['content']['reelItemRenderer']['videoId']}"
+                        ),
+                        videos
+                    )
+                ),
+            ),
+            continuation,
+        )
+
+    @property
+    def shorts(self) -> Iterable[YouTube]:
+        """Yields YouTube objects of shorts in this playlist
+
+        :rtype: List[YouTube]
+        :returns: List of YouTube
+        """
+        return DeferredGeneratorList(self.videos_generator(urls=self.short_urls))
+
+    @property  # type: ignore
+    @cache
+    def short_urls(self) -> DeferredGeneratorList:
+        """Complete links of all the shorts in playlist
+
+        :rtype: List[str]
+        :returns: List of video URLs
+        """
+        return DeferredGeneratorList(self.url_generator(extractor=self._extract_shorts,html=self.shorts_html))
